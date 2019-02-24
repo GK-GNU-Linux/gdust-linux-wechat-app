a8cfd511726a2a6a69c09f18571d351707d85788
dairoot <623815825@qq.com>
2019-02-24 12:41:04 +0800

    fix bug

--- pages/core/kb/kb.js
+++ pages/core/kb/kb.js
@@ -351,7 +351,6 @@ Page({
       var toweek = week;
       if (today === 6) {
         toweek = toweek - 1;
-        week = week - 1;
       }
       var lessons = _data.lessons.schedule;
       //各周日期计算
